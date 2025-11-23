42d0fe443dd1b265fae41e19611ca961943f71df
Rolly Maulana Awangga <awangga@gmail.com>
2025-11-23 08:13:35 +0700

    tambah package notif

--- go.mod
+++ go.mod
@@ -3,13 +3,29 @@ module github.com/n0z0/scp
 go 1.25.3
 
 require (
+	github.com/faiface/beep v1.1.0
+	github.com/gen2brain/beeep v0.11.1
 	github.com/n0z0/cachedb v0.1.6
 	github.com/pkg/sftp v1.13.10
 	golang.org/x/crypto v0.45.0
 )
 
 require (
+	git.sr.ht/~jackmordaunt/go-toast v1.1.2 // indirect
+	github.com/esiqveland/notify v0.13.3 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/jackmordaunt/icns/v3 v3.0.1 // indirect
 	github.com/kr/fs v0.1.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sergeymakinen/go-bmp v1.0.0 // indirect
+	github.com/sergeymakinen/go-ico v1.0.0-beta.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
---
+++ notif.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/wav"
+	"github.com/gen2brain/beeep"
+)
+
+//go:embed art/tot2wuk2.wav
+var notifWav []byte
+
+//go:embed art/indo.png
+var indoPng []byte
+
+var (
+	iconPath string
+	iconOnce sync.Once
+)
+
+// initIcon akan memastikan file PNG ada di disk (temp dir) tepat sekali
+func initIcon() (string, error) {
+	var err error
+	iconOnce.Do(func() {
+		tmpDir := os.TempDir()
+		iconPath = filepath.Join(tmpDir, "indo.png")
+
+		// cek sudah ada atau belum
+		if _, statErr := os.Stat(iconPath); os.IsNotExist(statErr) {
+			// belum ada → tulis
+			err = os.WriteFile(iconPath, indoPng, 0o644)
+		}
+	})
+	return iconPath, err
+}
+
+func NotifikasiDesktop(judul, pesan string) error {
+	path, err := initIcon()
+	if err != nil {
+		return err
+	}
+
+	return beeep.Notify(judul, pesan, path)
+}
+
+func PlayNotificationSound() {
+	// decode WAV dari byte yang di-embed
+	streamer, format, err := wav.Decode(bytes.NewReader(notifWav))
+	if err != nil {
+		log.Fatal("gagal decode wav:", err)
+	}
+	defer streamer.Close()
+
+	// init speaker sesuai format audio
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	if err != nil {
+		log.Fatal("gagal init speaker:", err)
+	}
+
+	done := make(chan struct{})
+
+	// play sampai selesai
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		close(done)
+	})))
+
+	<-done
+}
